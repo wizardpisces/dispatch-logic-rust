4c6d83c130da6045c5cf5d32d859ce49927203d3
wizardpisces <1275839779@qq.com>
2022-11-11 11:23:11 +0800

    chore: optimize panic error

--- src/constant.rs
+++ src/constant.rs
@@ -1,7 +1,4 @@
 use std::str::FromStr;
-// use std::error::Error;
-// use std::fmt;
-// impl Error for DispatchLogicType {}
 
 #[repr(u8)]
 #[derive(Debug,PartialEq)]
@@ -10,15 +7,25 @@ pub enum DispatchLogicType {
     Seller
 }
 
+// #[derive(Debug)]
+// pub struct CustomError{
+//     kind: ErrorType
+// }
+
+// #[derive(Debug)]
+// enum ErrorType{
+//     InvalidDispatchLogicType
+// }
 impl FromStr for DispatchLogicType {
 
-    type Err = ();
+    type Err = String;
 
     fn from_str(input: &str) -> Result<DispatchLogicType, Self::Err> {
         match input {
             "Normal"  => Ok(DispatchLogicType::Normal),
             "Seller"  => Ok(DispatchLogicType::Seller),
-            _      => Err(()),
+            // _      => Err(CustomError{kind:ErrorType::InvalidDispatchLogicType}),
+            _      => Err(String::from("unknown dispatch logic type")),
         }
     }
 }
